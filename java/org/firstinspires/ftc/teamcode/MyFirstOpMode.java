ad0a414e0dc5cc2443de8cab57669fc05bd83e39
ftc-6717 <58494123+ftc-6717@users.noreply.github.com>
2019-12-03 16:51:20 -0800

    Add files via upload

---
+++ java/org/firstinspires/ftc/teamcode/MyFirstOpMode.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Demonstrates empty OpMode
+ */
+@TeleOp(name = "Concept: NullOp", group = "Concept")
+public class MyFirstOpMode extends OpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+    }
+
+    /*
+     * Code to run when the op mode is first enabled goes here
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * This method will be called ONCE when start is pressed
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    /*
+     * This method will be called repeatedly in a loop
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
+     */
+    @Override
+    public void loop() {
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+    }
+}
