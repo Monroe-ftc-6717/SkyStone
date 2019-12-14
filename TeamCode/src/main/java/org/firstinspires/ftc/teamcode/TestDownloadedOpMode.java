8dbad47d4079ba1fa47094191b1f539f031e03c8
ftc-6717 <58494123+ftc-6717@users.noreply.github.com>
2019-12-14 07:36:42 -0800

    Create TestDownloadedOpMode.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDownloadedOpMode.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class TestDownloadedOpMode extends OpMode {
+    private DcMotor leftFront, rightFront, leftRear, rightRear;
+    @Override
+    public void init() {
+
+        leftFront = hardwareMap.dcMotor.get("l1");
+        rightFront = hardwareMap.dcMotor.get("r1");
+        leftRear = hardwareMap.dcMotor.get("l2");
+        rightRear = hardwareMap.dcMotor.get("r2");
+
+    }
+
+    @Override
+    public void loop() {
+        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+        double rightX = gamepad1.right_stick_x;
+        final double v1 = r * Math.cos(robotAngle) + rightX;
+        final double v2 = r * Math.sin(robotAngle) - rightX;
+        final double v3 = r * Math.sin(robotAngle) + rightX;
+        final double v4 = r * Math.cos(robotAngle) - rightX;
+
+        leftFront.setPower(v1);
+        rightFront.setPower(v2);
+        leftRear.setPower(v3);
+        rightRear.setPower(v4);
+    }
+}
